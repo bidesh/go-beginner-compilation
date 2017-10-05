cba6edbc19e42d77313ae67c4e3d8359d4ac6c45
ThaWolfie <melsgrapes@gmail.com>
2017-10-05 11:18:44 +0200

    Updated code with taking out to many brackets based on PR comment Bidesh

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 	"os"
 )
 
-func main() {
-	{
+func main() {	
 		if len(os.Args) > 1 {
 			fmt.Println("I am the guest")
 		} else {
 			fmt.Println("I am the host")
-		}
-	}
+		}	
 }
 
 //goimports to import the repo's required
 //gofmt to tidy the code
-//Bidesh update
